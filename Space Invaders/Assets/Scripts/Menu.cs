4e220c0e1a387942e3911d8f320af49c4d6b8b66
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-03-30 21:38:25 +0700

    Menu + SpaceStorage

---
+++ Space Invaders/Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void Awake()
+    {
+       
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenShop()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Space Invaders/Assets/Scripts/Storage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    
+    [System.Serializable]
+    public struct Ships
+    {
+        public string name;
+        public float Cost;
+        public float unlock;
+
+        public Ships(string namec, float Costc)
+        {
+            name = namec;
+            Cost = Costc;
+            unlock = 0;
+        }
+    }
+
+    public Ships[] PlayerShips;
+    
+    public void Awake()
+    {
+        
+    }
+}
