98df98dda925b7474451ee0b183927c5bd4c5864
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-04-01 22:56:01 +0700

    Movement + ShopMaket
    Player movement logic + shop maket in menu

---
+++ Space Invaders/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        float moveSpeed = 0.2f;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed);
+    }
+}
