7901d82ca32f2e9be5fc957b65b08faa7f1f7c2e
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-04-06 00:19:57 +0700

    Скрипт выстрела
    Выстрел по таймеру

---
+++ Space Invaders/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ShootingScript : MonoBehaviour
+{
+    public float AttackSpeed = 1;
+
+    private void Start()
+    {
+        InvokeRepeating("Shot", 0, AttackSpeed); // вызов выстрела
+    }
+
+    void Shot()
+    {
+        //тута будет спавн снаряда(у меня есть разные спрайты, будут для улучшений)
+        //дальше надо сюда же звук выстрела запихать
+    }
+}
