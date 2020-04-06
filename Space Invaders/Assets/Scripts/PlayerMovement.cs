6f65f7b42695a47bfef8ffac58b4e2a9a72fbcb9
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-04-06 21:56:28 +0700

    Начал делать движение через тач

--- Space Invaders/Assets/Scripts/PlayerMovement.cs
+++ Space Invaders/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,19 @@ using UnityEngine.Experimental.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void FixedUpdate()
+    [SerializeField]
+    float _velocity = 10;
+
+    void Update()
     {
         float moveSpeed = 0.2f;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed);
+        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Input.touches[0].position, Time.deltaTime * _velocity);
+        }
     }
 }
