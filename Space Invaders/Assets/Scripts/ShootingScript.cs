bdbf4dd1683812cfee46bd42131c8e81d185ac4e
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-04-06 16:46:19 +0700

    Добавил звук при выстреле
    пиу

--- Space Invaders/Assets/Scripts/ShootingScript.cs
+++ Space Invaders/Assets/Scripts/ShootingScript.cs
@@ -7,14 +7,18 @@ using UnityEngine.Experimental.UIElements;
 public class ShootingScript : MonoBehaviour
 {
     public float AttackSpeed = 1;
+    public AudioClip piu;
+    private AudioSource audio;
 
     private void Start()
     {
+        audio = GetComponent<AudioSource>();
         InvokeRepeating("Shot", 0, AttackSpeed); // вызов выстрела
     }
 
     void Shot()
     {
+        audio.PlayOneShot(piu);
         //тута будет спавн снаряда(у меня есть разные спрайты, будут для улучшений)
         //дальше надо сюда же звук выстрела запихать
     }
