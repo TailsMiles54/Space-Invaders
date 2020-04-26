e4c4148b0a5ddf1556430e0a25b40fb7b5df9cad
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-04-27 01:44:32 +0700

    ShopItems
    Сard purchase of ships(as long as no functionality, only information)

---
+++ Space Invaders/Assets/Scripts/Shop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+   public GameObject ShopPanel, ShopMenu, Storage;
+   private Storage ShopStorage;
+   private Vector3 shopPanelTransform;
+
+   public void Start()
+   {
+      ShopStorage = Storage.GetComponent<Storage>();
+      LoadShop();
+   }
+
+   void LoadShop()
+   {
+      shopPanelTransform = ShopMenu.transform.position + new Vector3(0, 500, 0);
+      for (int i = 0; i < ShopStorage.ShopShipsStorage.Length; i++)
+      {
+         GameObject ShopElement = Instantiate(ShopPanel);
+         ShopElement.transform.SetParent(ShopMenu.transform,false);
+         ShopElement.transform.position = shopPanelTransform;
+         if (ShopStorage.ShopShipsStorage[i].ShopShipsUnlock == false){
+            ShopElement.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Купить";
+         }
+         if (ShopStorage.ShopShipsStorage[i].ShopShipsUnlock == true) {
+            ShopElement.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = "Выбрать";
+         }
+         
+         ShopElement.transform.GetChild(1).GetComponent<Text>().text = ShopStorage.ShopShipsStorage[i].ShopShipsName;
+         ShopElement.transform.GetChild(2).GetComponent<Text>().text = "Цена: " + ShopStorage.ShopShipsStorage[i].ShopShipsCost;
+         ShopElement.transform.GetChild(3).GetComponent<Image>().sprite = ShopStorage.ShopShipsStorage[i].ShopShipsPrefab.GetComponent<SpriteRenderer>().sprite;
+         
+         shopPanelTransform -= new Vector3(0, 600, 0);
+      }
+   }
+}
--- Space Invaders/Assets/Scripts/Storage.cs
+++ Space Invaders/Assets/Scripts/Storage.cs
@@ -10,6 +10,15 @@ using UnityEngine;
 public class Storage : MonoBehaviour
 {
     [Serializable]
+    public struct ShopShips
+    {
+        public string ShopShipsName;
+        public float ShopShipsCost;
+        public bool ShopShipsUnlock;
+        public GameObject ShopShipsPrefab;
+    }
+    
+    [Serializable]
     public struct Ships
     {
         public string name;
@@ -23,6 +32,7 @@ public class Storage : MonoBehaviour
         }
     }
 
+    public ShopShips[] ShopShipsStorage; 
     public Ships[] PlayerShips;
 
     public void Start()
@@ -39,6 +49,13 @@ public class Storage : MonoBehaviour
 
     void SaveShips()
     {
+        for (int i = 0; i < PlayerShips.Length; i++)
+        {
+            PlayerShips[i].name = ShopShipsStorage[i].ShopShipsName;
+            PlayerShips[i].Cost = ShopShipsStorage[i].ShopShipsCost;
+            PlayerShips[i].unlock = ShopShipsStorage[i].ShopShipsUnlock;
+        }
+        
         string ToJSONShips = JsonConvert.SerializeObject(PlayerShips);
         PlayerPrefs.SetString("PlayerShips", ToJSONShips);
         Debug.Log(ToJSONShips);
@@ -47,8 +64,19 @@ public class Storage : MonoBehaviour
     void LoadShips()
     {
         string FromJSONShips = PlayerPrefs.GetString("PlayerShips");
-        Debug.Log(FromJSONShips);
         PlayerShips = JsonConvert.DeserializeObject<Ships[]>(FromJSONShips);
+        
+        for (int i = 0; i < PlayerShips.Length; i++)
+        {
+           ShopShipsStorage[i].ShopShipsName =  PlayerShips[i].name;
+           ShopShipsStorage[i].ShopShipsCost =  PlayerShips[i].Cost;
+           ShopShipsStorage[i].ShopShipsUnlock =  PlayerShips[i].unlock;
+        }
+    }
+
+    public void SaveStorage()
+    {
+        SaveShips();
     }
 }
 // Пытаюсь сделать хранение кулпенных игроком кораблей
